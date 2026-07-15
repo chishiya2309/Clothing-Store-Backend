b1cba5adf269d04d56376b16d80634df73944dee
Lê Quang Hưng <141334116+chishiya2309@users.noreply.github.com>
2026-07-15 16:58:23 +0700

    Cấu hình thêm và ghi log rõ phục vụ fixbug production (#81)

--- src/main/java/vn/hcmute/edu/dp/nhom10/backend/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/vn/hcmute/edu/dp/nhom10/backend/security/JwtAuthenticationEntryPoint.java
@@ -4,6 +4,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -13,6 +14,7 @@ import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     private final ApiErrorResponseWriter apiErrorResponseWriter;
@@ -20,6 +22,7 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
             throws IOException, ServletException {
+        log.error("Unauthorized error: {} for request URI: {}", authException.getMessage(), request.getRequestURI());
         apiErrorResponseWriter.write(
                 response,
                 HttpStatus.UNAUTHORIZED,
