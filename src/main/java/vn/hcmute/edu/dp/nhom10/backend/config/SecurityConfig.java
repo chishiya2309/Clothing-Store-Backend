05eebd76ddd57ba313ee8f39028ea4dec312a7c3
chishiya2309 <lequanghung.work@gmail.com>
2026-07-15 14:22:55 +0700

    Thêm các file cấu hình phục vụ deploy

--- src/main/java/vn/hcmute/edu/dp/nhom10/backend/config/SecurityConfig.java
+++ src/main/java/vn/hcmute/edu/dp/nhom10/backend/config/SecurityConfig.java
@@ -51,6 +51,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**", "/api/guest/**", "/error").permitAll()
+                        .requestMatchers("/actuator/health").permitAll()
                         .requestMatchers(HttpMethod.GET,
                                 "/sitemap.xml",
                                 "/robots.txt",
